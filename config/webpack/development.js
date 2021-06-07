566e4d82876e38c096ed8733e79d0e4df27bc461
August <agostino.gong@gmail.com>
2021-06-07 20:10:43 +0200

    clean up layout

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,13 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-const webpack = require("webpacker")
-
-environment.plugins.append("Provide", new webpack.PovidePlugins({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-}))
-
-
 module.exports = environment.toWebpackConfig()
